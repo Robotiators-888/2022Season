22072e5f06c62dd900d714de959c107d83ae9e80
Ryan <ryanpcather@gmail.com>
2022-01-17 12:10:36 -0500

    goofed previous commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,15 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    //Drivetrain Can IDs
+    public static final int ID_LEFT_PRIMARY = 20;  
+    public static final int ID_LEFT_SECONDARY = 21;  
+    public static final int ID_RIGHT_PRIMARY = 22;  
+    public static final int ID_RIGHT_SECONDARY = 23;  
+
+    //Kinematics
+    public static final double TRACKWIDTH = 30;  //track width in inches
+    
+
+}
