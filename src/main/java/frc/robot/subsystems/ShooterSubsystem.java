e74b431630477cbcbd72645e420fbdf6502d47d8
Jokorie <Jonathan.c.okorie@gmail.com>
2022-01-29 14:21:53 -0500

    Update ShooterSubsystem.java

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,16 +5,31 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
+  double ShooterSpeed;
+  CANSparkMax ShooterMotor = new CANSparkMax(Constants.SHOOTER_MOTOR_ID, MotorType.kBrushless);
+  
+
+  
   public ShooterSubsystem() {
 
   }
 
 
   public void elevatorUp(){
-    
+    ShooterSpeed = Constants.SHOOTER_SPEED;
+    ShooterMotor.set(ShooterSpeed);
+  }
+  
+  public void ShooterStop(){
+  ShooterMotor.set(0.0);
   }
 
   @Override
