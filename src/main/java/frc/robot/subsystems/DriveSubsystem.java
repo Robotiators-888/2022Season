c9d208c414c80583294519452043e16f3610ecd4
Spencer McGrath <Samcgrath2005@gmail.com>
2022-02-03 14:55:11 -0500

    2/3/22
    Implemented drivetrain code
    
    waiting on vision to be added, testing drive code asap

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,45 +5,45 @@
 */
 
 package frc.robot.subsystems;
+
 import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
-
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
-
-
-
-
-public class DriveSubsystem extends SubsystemBase{
+public class DriveSubsystem extends SubsystemBase {
 
     // Left
-    private final WPI_VictorSPX frontLeft = new WPI_VictorSPX(Constants.FRONT_LEFT_ID);
-    private final WPI_VictorSPX rearLeft = new WPI_VictorSPX(Constants.REAR_LEFT_ID);
-    public final SpeedControllerGroup leftGroup = new SpeedControllerGroup(frontLeft, rearLeft);
+    private final CANSparkMax Left_1 = new CANSparkMax(Constants.Drive_Left_1, MotorType.kBrushless);
+    private final CANSparkMax Left_2 = new CANSparkMax(Constants.Drive_Left_2, MotorType.kBrushless);
+    private final CANSparkMax Left_3 = new CANSparkMax(Constants.Drive_Left_3, MotorType.kBrushless);
 
-    // Right 
-    private final WPI_VictorSPX frontRight = new WPI_VictorSPX(Constants.FRONT_RIGHT_ID);
-    private final WPI_VictorSPX rearRight = new WPI_VictorSPX(Constants.REAR_RIGHT_ID);
-    public final SpeedControllerGroup rightGroup = new SpeedControllerGroup(frontRight,rearRight);
+    public final MotorControllerGroup leftGroup = new MotorControllerGroup(Left_1, Left_2, Left_3);
 
+    // Right
+    private final CANSparkMax Right_1 = new CANSparkMax(Constants.Drive_Right_1, MotorType.kBrushless);
+    private final CANSparkMax Right_2 = new CANSparkMax(Constants.Drive_Right_2, MotorType.kBrushless);
+    private final CANSparkMax Right_3 = new CANSparkMax(Constants.Drive_Right_3, MotorType.kBrushless);
 
-    
+    public final MotorControllerGroup rightGroup = new MotorControllerGroup(Right_1, Right_2, Right_3);
+
     public final MecanumDrive driveTrain = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
-    public DriveSubsystem(){
+    public DriveSubsystem() {
         rearLeft.setInverted(true);
         frontRight.setInverted(true);
     }
 
-    public void drive(double ySpeed, double xSpeed,double zRotation){
-        // ySpeed, xSpeed, and zRotation are all supplier values from the joystick object
-        driveTrain.driveCartesian(ySpeed,xSpeed,zRotation);
+    public void drive(double ySpeed, double xSpeed, double zRotation) {
+        // ySpeed, xSpeed, and zRotation are all supplier values from the joystick
+        // object
+        driveTrain.driveCartesian(ySpeed, xSpeed, zRotation);
     }
-    
+
 }
