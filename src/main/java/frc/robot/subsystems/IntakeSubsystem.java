8a1c3b0f0b6939a36382b8323fda1e1595c417f1
Kristin <kristin.bezick@gmail.com>
2022-02-09 11:42:56 -0800

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,14 +1,58 @@
 package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.MotorFeedbackSensor;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-TalonSRX frontIntake = new TalonSRX(ControlMode.kforward, Constants.FRONT_INTAKE_MOTOR_ID);
+TalonSRX frontIntake = new TalonSRX(Constants.FRONT_INTAKE_MOTOR_ID);
+TalonSRX middleIntake = new TalonSRX(Constants.MIDDLE_INTAKE_MOTOR_ID);
 
-
-public void IntakeSubsystem()
+//determines wether ball is coming from the front or back
+boolean inFront;
+//power of the motor when active
+double motorPower = 1.0;
+//take double parameter as the percentage of power given to each motor
+public void SetMotors(double front, double middle) 
 {
+    frontIntake.set(TalonSRXControlMode.PercentOutput, front);
+    middleIntake.set(TalonSRXControlMode.PercentOutput, middle);
+    
+}
+public void StopAllMotors()
+{
+ SetMotors(0, 0);
+};
+public void Intake()
+{
+    SetMotors(motorPower,0);
+    System.out.println("Intaking...");
+}
 
+public void Outtake()
+{
+    SetMotors(0, -motorPower);
+    System.out.println("Spitting Out!");
 }
+public void Index()
+{
+SetMotors(0, motorPower);
 }
+public void IntakeSubsytem(boolean ballDetected, boolean isTeamColor)
+{
+    //ball is detected in front of robot
+    while(ballDetected == true)
+    {
+        Intake();
+        while(isTeamColor == true)
+        {
+            Index();
+        }
+    }
+}
+}  
+
