40ec7599741cae0d784eeb3a83ae582806514791
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-02-11 09:57:11 -0500

    Added Index Subsystem
    - Added an index subsystem that builds.
    - Fixed MUX Error
    - Added getPosition(), getColor(), feed(), and all setSpeed functions.
    - Added various placeholder constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,9 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final int FRONT_INDEX_ID = 10;
+    public static final int TOWER_INDEX_ID = 11;
+    public static final int BACK_INDEX_ID = 12;
+}
