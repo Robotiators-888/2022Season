c7ca9d2bb4f971045d421f5ffeeabab7b44b9eef
Liam Williams <liam.a.wms@gmail.com>
2022-02-12 11:00:59 -0500

    Format, cleanup, and document IntakeSubsystem.

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,52 +1,76 @@
 package frc.robot.subsystems;
-import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import com.revrobotics.MotorFeedbackSensor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value; 
-
-
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import frc.robot.Constants;
 
+/**
+ * This subsystem represents the intake system on the robot, it will set the
+ * motor speed in percent and set pistons to on or off
+ */
 public class IntakeSubsystem extends SubsystemBase {
-//PS: SillyPandaDog is also Jokorie
-//Initialize Intake Motors
-TalonSRX Intake = new TalonSRX(Constants.MOTOR_ID);
+    // PS: SillyPandaDog is also Jokorie
+    // Initialize Intake Motors
+    TalonSRX Intake = new TalonSRX(Constants.MOTOR_ID);
 
-//power of the motor when active
-double motorPower = 0.0;
-//take double parameter as the percentage of power given to each motor
-DoubleSolenoid solenoid1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
-DoubleSolenoid solenoid2 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 2, 3);
-public boolean intakeGet() {
-    return (solenoid1.get().equals(DoubleSolenoid.Value.kForward) && solenoid2.get().equals(DoubleSolenoid.Value.kForward)) {
-}
-public void intakeSet(boolean pistonState) { //up false, down true
-    if (pistonState) {
-        solenoid1.set(DoubleSolenoid.Value.kForward);
-        solenoid2.set(DoubleSolenoid.Value.kForward);
+    // take double parameter as the percentage of power given to each motor
+    DoubleSolenoid solenoid1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
+    DoubleSolenoid solenoid2 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 2, 3);
 
-    } else {
-        solenoid1.set(DoubleSolenoid.Value.kReverse);
-        solenoid2.set(DoubleSolenoid.Value.kReverse);
+    /**
+     * Gets the status of the intake pistons.
+     * 
+     * @returna boolean true if pistons are on, else false
+     */
+    public boolean intakeGet() {
+        return (solenoid1.get().equals(DoubleSolenoid.Value.kForward)
+                && solenoid2.get().equals(DoubleSolenoid.Value.kForward));
+    }
+
+    /**
+     * Sets the state of the intake pistons.
+     * 
+     * @param pistonState sets Pistons to on or off depending on the pistonState, if
+     *                    the intake is up, it will be false, if intake is down, it
+     *                    will be false
+     */
+    public void intakeSet(boolean pistonState) { // up false, down true
+        if (pistonState) {
+            solenoid1.set(DoubleSolenoid.Value.kForward);
+            solenoid2.set(DoubleSolenoid.Value.kForward);
+
+        } else {
+            solenoid1.set(DoubleSolenoid.Value.kReverse);
+            solenoid2.set(DoubleSolenoid.Value.kReverse);
+
+        }
+    }
+
+    /**
+     * Gets the percent output speed of the intake motor.
+     * 
+     * @return the power of the motor control in percent, from -1 to 1 or -100% to
+     *         100%
+     */
+    public double intakeSpeedGet() {
+        return Intake.getMotorOutputPercent();
 
     }
-}
-public double intakeSpeedGet() {
-    return Intake.getMotorOutputPercent(); 
 
-}
-/**
- *  @param percentSpeed takes in a double value between -1 and 1, representing the motor power in percent, or from -100% to 100% 
- */
-public void intakeSpeedSet(double percentSpeed) {
-    
-    Intake.set(TalonSRXControlMode.PercentOutput, percentSpeed); 
-}
+    /**
+     * Set the percent output speed of the intake motor.
+     * 
+     * @param percentSpeed takes in a double value between -1 and 1, representing
+     *                     the motor power in percent, or from -100% to 100%
+     */
+    public void intakeSpeedSet(double percentSpeed) {
+
+        Intake.set(TalonSRXControlMode.PercentOutput, percentSpeed);
+    }
 
 }
