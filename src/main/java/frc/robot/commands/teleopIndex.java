bcd0e58e339184ffdb1a82860c8874d452532cc6
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-02-12 12:11:56 -0500

    Added command to test Index

---
+++ src/main/java/frc/robot/commands/teleopIndex.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.IndexSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class teleopIndex extends CommandBase {
+
+  private IndexSubsystem index;
+  /** Creates a new telopIndex. */
+  public teleopIndex(IndexSubsystem indexArgs) {
+
+    this.index = indexArgs;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(index);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    index.feed();
+    index.setSpeedBack(0.1);
+    index.setSpeedFront(0.1);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    index.stopFeed();
+    index.setSpeedBack(0);
+    index.setSpeedFront(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
