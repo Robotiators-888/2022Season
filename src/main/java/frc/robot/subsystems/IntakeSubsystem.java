c5780ed1b1f49e7998c2e91ccd67ec4fb45185b4
Georgetheasian <80559769+Georgetheasian@users.noreply.github.com>
2022-02-12 10:27:17 -0500

    Intake init

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,86 +1,51 @@
 package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Joystick;
+import com.revrobotics.MotorFeedbackSensor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
-import edu.wpi.first.wpilibj.XboxController;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value; 
+
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
+//PS: SillyPandaDog is also Jokorie
+//Initialize Intake Motors
+TalonSRX Intake = new TalonSRX(Constants.MOTOR_ID);
 
-TalonSRX intakeMotor;
-XboxController controller;
-
-//Initialize Piston Solenoids (forward and backward)
-DoubleSolenoid intakePistonR = new DoubleSolenoid(9, PneumaticsModuleType.REVPH, Constants.SOLENOID_FRIGHT_ID, Constants.SOLENOID_BRIGHT_ID);
-DoubleSolenoid intakePistonL = new DoubleSolenoid(9, PneumaticsModuleType.REVPH, Constants.SOLENOID_FLEFT_ID, Constants.SOLENOID_BLEFT_ID);
-
-//power of the motor when active (percentage)
-double motorPower = 1.0;
-boolean isExtended = false;
-
-public IntakeSubsystem (int intakeMotor, int controller)
-{ 
-this.intakeMotor = new TalonSRX(intakeMotor);
-this.controller = new XboxController(controller);
-}
-public void Set()
-{
-    intakeMotor.set(TalonSRXControlMode.PercentOutput, motorPower);
-}
+//power of the motor when active
+double motorPower = 0.0;
 //take double parameter as the percentage of power given to each motor
-public void intakeSpeedSet(int speed)
-{
-    motorPower = speed;
-}
-
-public double intakeSpeedGet()
-{
-    return motorPower;
-}
-public void intakeSet(boolean extend)
-{
-    if(extend == true)
-    {
-    intakePistonL.set(kForward);
-    intakePistonR.set(kForward);
-    } else 
-    {
-        intakePistonL.set(kReverse);
-        intakePistonR.set(kReverse);
+DoubleSolenoid solenoid1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
+DoubleSolenoid solenoid2 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 2, 3);
+public boolean intakeGet() {
+    if (solenoid1.get().equals(DoubleSolenoid.Value.kForward) && solenoid2.get().equals(DoubleSolenoid.Value.kForward)) {
+        return true; 
+    } else {
+        return false; 
     }
 }
-public void ToggleIntakeArms()
-{
-    intakePistonL.toggle();
-    intakePistonR.toggle();
+public void intakeSet(boolean pistonState) { //up false, down true
+    if (intakeGet()) {
+        pistonState == true; 
+    } else {
+        pistonState == false; 
+    }
 }
-public void Stop()
-{
-    intakeMotor.set(TalonSRXControlMode.PercentOutput, 0);
-};
-public void Intake()
-{
-    motorPower = 1;
-    System.out.println("Intaking...");
+public double intakeSpeedGet() {
+    motorPower = TalonSRXControlMode.PercentOutput; 
+    return motorPower; 
 }
 
-public void Outtake()
-{
-    motorPower = -1;
-    System.out.println("Spitting Out!");
-}
-
-public boolean intakeGet()
-{
-    return isExtended;
-}
-public void IntakeSubsytem()
-{
+public void intakeSpeedSet(double percentSpeed) {
     
+    Intake.set(TalonSRXControlMode.PercentOutput, percentSpeed); 
+}
 
 }
-}  
-
