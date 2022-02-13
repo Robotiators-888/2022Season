b4c68f5ca2418e86c7f1731285f00c7956ef4a22
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-02-13 10:20:15 -0500

    Fixed belt direction

--- src/main/java/frc/robot/commands/teleopIndex.java
+++ src/main/java/frc/robot/commands/teleopIndex.java
@@ -30,6 +30,8 @@ public class teleopIndex extends CommandBase {
   @Override
   public void execute() {
     index.setSpeedTower(0.75);
+    index.setSpeedBack(-0.75);
+    index.setSpeedFront(-0.75);
   }
 
   // Called once the command ends or is interrupted.
