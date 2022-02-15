79fd42b53a63819977c3776dd2de3aaf07e6bea9
Zoe D <59486139+zdonal1573@users.noreply.github.com>
2022-02-15 18:47:40 -0500

    Shuffleboard data added

--- src/main/java/frc/robot/commands/ShooterSpin.java
+++ src/main/java/frc/robot/commands/ShooterSpin.java
@@ -31,6 +31,7 @@ public class ShooterSpin extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    
     shoot.setSpeed(-(1-joystick.getRawAxis(3))/2);
   }
 
