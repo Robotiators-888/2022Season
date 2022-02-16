2712b83f754718ddbf59344e16d512eae21ddb4c
Zoe D <59486139+zdonal1573@users.noreply.github.com>
2022-02-16 11:00:45 -0500

    Intake :D

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -146,6 +146,7 @@ public class RobotContainer {
         rightShoulder.whileHeld(new OuttakeMotorTest(m_intake));
         yButton.whileHeld(new teleopIndex(index));
         aButton.whileHeld(new ShooterSpin(shoot, twiststick));
+        bButton.whileHeld(new IntakeMotorTest(m_intake));
 
     }
 
