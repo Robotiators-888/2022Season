3a0d3209a6ba6f849ccb584e3b3d5cdc4d6817e0
Zoe D <59486139+zdonal1573@users.noreply.github.com>
2022-02-16 10:36:36 -0500

    Fixed Missing RobotContainer Things

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,27 +6,28 @@ package frc.robot;
 
 import java.util.List;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.commands.teleopDrive;
 import frc.robot.commands.teleopIndex;
 import frc.robot.commands.zeroHeading;
-
+import frc.robot.subsystems.ColorSensorSubsystem;
 import frc.robot.subsystems.Drivetrain;
-
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.UDP.BallDataPacket;
 import frc.robot.UDP.GenericBuffer;
@@ -45,6 +46,9 @@ import frc.robot.commands.IntakeMotorTest;
 import frc.robot.commands.PistonOutCmd;
 import frc.robot.commands.ShooterSpin;
 import frc.robot.commands.PistonInCmd;
+import frc.robot.commands.indexCanalOut;
+import frc.robot.commands.teleopCanal;
+import frc.robot.commands.teleopIndex;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -56,102 +60,116 @@ import frc.robot.commands.PistonInCmd;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // The robot's subsystems and commands are defined here...
-    private final GenericBuffer<BallDataPacket> ballBuffer = new GenericBuffer<>();
-    private final GenericBuffer<LimelightDataPacket> limelightBuffer = new GenericBuffer<>();
-    private final UDPReciever<BallDataPacket> m_BallReciever = new UDPReciever<>(Constants.BALL_PORT,
-            () -> new BallDataPacket(), ballBuffer);
-    private final UDPReciever<LimelightDataPacket> m_limelightReciever = new UDPReciever<>(Constants.LIMELIGHT_PORT,
-            () -> new LimelightDataPacket(), limelightBuffer);
+  // The robot's subsystems and commands are defined here...
 
-    private final Field2d field2d = new Field2d();
+  // public final static Drivetrain m_driveSubsystem = new Drivetrain();
+  private final GenericBuffer<BallDataPacket> ballBuffer = new GenericBuffer<>();
+  private final GenericBuffer<LimelightDataPacket> limelightBuffer = new GenericBuffer<>();
+  private final UDPReciever<BallDataPacket> m_BallReciever = new UDPReciever<>(Constants.BALL_PORT,
+      () -> new BallDataPacket(), ballBuffer);
+  private final UDPReciever<LimelightDataPacket> m_limelightReciever = new UDPReciever<>(Constants.LIMELIGHT_PORT,
+      () -> new LimelightDataPacket(), limelightBuffer);
 
-    // subsystems
-    private Limelight m_limelight = new Limelight();
-    private Shooter shoot = new Shooter();
-    private Index m_index = new Index();
-    private Drivetrain drivetrain = new Drivetrain(field2d);
-    private IntakeSubsystem m_intake = new IntakeSubsystem();
-    private IndexSubsystem index = new IndexSubsystem();
-    private Autonomous autoHelper = new Autonomous(drivetrain);
+  private Limelight m_limelight = new Limelight();
+  private Shooter shoot = new Shooter();
+  private Index m_index = new Index();
+  private IntakeSubsystem m_intake = new IntakeSubsystem();
 
-    // Joystick objects
-    private Joystick joystick = new Joystick(Constants.JOYSTICK_PORT);
+  // private ColorSensorSubsystem colorSensor = new ColorSensorSubsystem();
+  private IndexSubsystem index = new IndexSubsystem();
+  private CanalSubsystem canal = new CanalSubsystem();
 
-    JoystickButton aButton = new JoystickButton(joystick, 1);
-    JoystickButton bButton = new JoystickButton(joystick, 2);
-    JoystickButton xButton = new JoystickButton(joystick, 3);
-    JoystickButton yButton = new JoystickButton(joystick, 4);
-    JoystickButton leftShoulder = new JoystickButton(joystick, 5);
-    JoystickButton rightShoulder = new JoystickButton(joystick, 6);
-    JoystickButton thumbLeft = new JoystickButton(joystick, 7);
-    JoystickButton thumbRight = new JoystickButton(joystick, 8);
+  private final Field2d field2d = new Field2d();
+  private Drivetrain drivetrain = new Drivetrain(field2d);
 
-    // Auto objects
-    SendableChooser<Command> chooser = new SendableChooser<>();
+  private Joystick joystick = new Joystick(Constants.JOYSTICK_PORT);
+  private Joystick twiststick = new Joystick(Constants.TWISTSTICK_PORT);
+
+  JoystickButton aButton = new JoystickButton(joystick, 1);
+  JoystickButton bButton = new JoystickButton(joystick, 2);
+  JoystickButton xButton = new JoystickButton(joystick, 3);
+  JoystickButton yButton = new JoystickButton(joystick, 4);
+  JoystickButton leftShoulder = new JoystickButton(joystick, 5);
+  JoystickButton rightShoulder = new JoystickButton(joystick, 6);
+  JoystickButton backButton = new JoystickButton(joystick,7);
+  JoystickButton startButton = new JoystickButton(joystick, 8);
+  JoystickButton thumbLeft = new JoystickButton(joystick, 9);
+  JoystickButton thumbRight = new JoystickButton(joystick, 10);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+
+    // m_BallReciever.start();
+    // m_limelightReciever.start();
+
+    // periodic getting
+    // separate function > getting string value
+
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    drivetrain.setDefaultCommand(new teleopDrive(drivetrain, () -> joystick.getRawAxis(Constants.LEFT_AXIS),
+        () -> joystick.getRawAxis(Constants.RIGHT_AXIS)));
+   // xButton.whenPressed(new zeroHeading(drivetrain));
+    thumbLeft.whenPressed(new PistonInCmd(m_intake));
+    thumbRight.whenPressed(new PistonOutCmd(m_intake));
+    // While back button is pressed, run autoshoot command
+    backButton.whileHeld(new LimelightCommand(m_limelight, shoot, m_index));
+    // While start button is pressed, run autoaim command
+    startButton.whileHeld(new Aim(m_limelight, drivetrain));
+    
+    rightShoulder.whileHeld(new OuttakeMotorTest(m_intake));
+    xButton.whileHeld(new teleopCanal(canal));
+    yButton.whileHeld(new teleopIndex(index));
+    aButton.whileHeld(new ShooterSpin(shoot, twiststick));
+    bButton.whileHeld(new IntakeMotorTest(m_intake)); 
+    leftShoulder.whileHeld(new indexCanalOut(canal, index));
+  }
+
+  public Command getAutonomousCommand() {
+    var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(new SimpleMotorFeedforward(Constants.ksVolts,
+        Constants.kvVoltSecondsPerMeter, Constants.kaVoltSecondsSquaredPerMeter), Constants.kDriveKinematics, 10);
+
     TrajectoryConfig config = new TrajectoryConfig(Constants.kMaxSpeedMetersPerSecond,
-            Constants.kMaxAccelerationMetersPerSecondSquared).setKinematics(Constants.kDriveKinematics);
-    Trajectory ballin1 = autoHelper.getTrajectory("paths/test2balll.wpilib.json");
-    Trajectory ballin2 = autoHelper.getTrajectory("paths/test2balll_0.wpilib.json");
-    Trajectory Str8 = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)),
-            List.of(), new Pose2d(3, 0, new Rotation2d(0)), config);
+        Constants.kMaxAccelerationMetersPerSecondSquared).setKinematics(Constants.kDriveKinematics)
+            .addConstraint(autoVoltageConstraint);
 
-    // Auto command groups
-    Command straightAuto = new SequentialCommandGroup(
-            new InstantCommand(() -> drivetrain.setPosition(0, 0, new Rotation2d(0))),
-            autoHelper.getRamset(Str8).andThen(() -> drivetrain.tankDriveVolts(0, 0)));
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(2, 2, new Rotation2d(0)),
+        List.of(), new Pose2d(5, 2, new Rotation2d(0)), config);
 
-    Command pwtest = new SequentialCommandGroup(
-            new InstantCommand(() -> drivetrain.setPosition(ballin1.getInitialPose())),
-            autoHelper.getRamset(ballin1),
-            autoHelper.getRamset(ballin2).andThen(() -> drivetrain.tankDriveVolts(0, 0)));
+    RamseteCommand ramseteCommand = new RamseteCommand(exampleTrajectory, drivetrain::getPose,
+        new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta),
+        new SimpleMotorFeedforward(Constants.ksVolts, Constants.kvVoltSecondsPerMeter,
+            Constants.kaVoltSecondsSquaredPerMeter),
+        Constants.kDriveKinematics, drivetrain::getWheelSpeeds,
+        new PIDController(Constants.kPDriveVel, 0, 0),
+        new PIDController(Constants.kPDriveVel, 0, 0),
+        drivetrain::tankDriveVolts, drivetrain);
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        // Configure the button bindings
-        configureButtonBindings();
+    field2d.getObject("traj").setTrajectory(exampleTrajectory);
 
-        chooser.setDefaultOption("Simple Auto", straightAuto);
-        chooser.addOption("Complex Auto", pwtest);
+    drivetrain.zeroHeading();
+    drivetrain.setPosition(2, 2, new Rotation2d(0));
 
-        m_BallReciever.start();
-        m_limelightReciever.start();
-
-        thumbLeft.whenPressed(new PistonInCmd(m_intake));
-        thumbRight.whenPressed(new PistonOutCmd(m_intake));
-        rightShoulder.whileHeld(new OuttakeMotorTest(m_intake));
-        yButton.whileHeld(new teleopIndex(index));
-        bButton.whileHeld(new IntakeMotorTest(m_intake));
-
-    }
-
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-     * it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
-    private void configureButtonBindings() {
-        drivetrain.setDefaultCommand(new teleopDrive(drivetrain, () -> joystick.getRawAxis(Constants.LEFT_AXIS),
-                () -> joystick.getRawAxis(Constants.RIGHT_AXIS)));
-        xButton.whenPressed(new zeroHeading(drivetrain));
-        thumbLeft.whenPressed(new PistonInCmd(m_intake));
-        thumbRight.whenPressed(new PistonOutCmd(m_intake));
-        // While a button is pressed, run autoshoot command
-        aButton.whileHeld(new LimelightCommand(m_limelight, shoot, m_index));
-        // While b button is pressed, run autoaim command
-        bButton.whileHeld(new Aim(m_limelight, drivetrain));
-        leftShoulder.whileHeld(new IntakeMotorTest(m_intake));
-        rightShoulder.whileHeld(new OuttakeMotorTest(m_intake));
-        yButton.whileHeld(new teleopIndex(index));
-    }
-
-    public Command getAutonomousCommand() {
-        return chooser.getSelected();
-    }
+    return ramseteCommand.andThen(() -> drivetrain.tankDriveVolts(0, 0));
+  }
 
 }
+
+/**
+ * Use this to pass the autonomous command to the main {@link Robot} class.
+ *
+ * @return the command to run in autonomous
+ */
