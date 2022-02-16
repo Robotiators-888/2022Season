bac1439d9a3c36fa6a951675f06a40984d3d53cc
888robotiators <Samcgrath2005@gmail.com>
2022-02-15 19:30:32 -0500

    added manual shooter cmd

---
+++ src/main/java/frc/robot/commands/ShooterRPMs.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class ShooterRPMs extends CommandBase {
+  /** Creates a new ShooterRPMs. */
+  private Shooter shoot;
+  public ShooterRPMs(Shooter subsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.shoot = subsystem;
+    addRequirements(shoot);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    shoot.setRPM(-3824);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shoot.setSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
