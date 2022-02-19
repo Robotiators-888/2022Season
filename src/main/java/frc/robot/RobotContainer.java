c551e47ab32d72a74476c62a4aefc12423e6841e
Ryan <ryanpcather@gmail.com>
2022-02-19 16:34:45 -0500

    some fixes after merge

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -41,11 +41,9 @@ import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.commands.OuttakeMotorTest;
-import frc.robot.commands.IntakeMotorTest;
-import frc.robot.commands.PistonOutCmd;
-import frc.robot.commands.PistonInCmd;
-
+import frc.robot.commands.IntakeSpin;
+import frc.robot.commands.ShooterSpin;
+import frc.robot.commands.canalRun;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 /**
