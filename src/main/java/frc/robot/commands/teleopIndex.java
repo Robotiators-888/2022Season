e52e2e753023700c0721b8e6ef872f9ffe2f46f4
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-02-19 12:33:39 -0500

    Fixed merge conflicts

--- src/main/java/frc/robot/commands/teleopIndex.java
+++ src/main/java/frc/robot/commands/teleopIndex.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.IndexSubsystem;
 
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 
@@ -13,6 +14,7 @@ public class teleopIndex extends CommandBase {
 
   private IndexSubsystem index;
   private boolean isDone = false;
+  
   /** Creates a new teleopIndex. */
   public teleopIndex(IndexSubsystem indexArgs) {
     // Use addRequirements() here to declare subsystem dependencies.
