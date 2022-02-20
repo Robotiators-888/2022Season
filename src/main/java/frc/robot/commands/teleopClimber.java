1166bd6a4fd65144bc702ea884eb1a8b44f8ba46
888robotiators <Samcgrath2005@gmail.com>
2022-02-20 12:51:48 -0500

    added end of command

--- src/main/java/frc/robot/commands/teleopClimber.java
+++ src/main/java/frc/robot/commands/teleopClimber.java
@@ -35,7 +35,9 @@ public class teleopClimber extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    climber.speedSet(0);
+  }
 
   // Returns true when the command should end.
   @Override
