83889ea232589570fc39cfd85ee862af5f45c235
888robotiators <Samcgrath2005@gmail.com>
2022-02-20 14:10:16 -0500

    need to finish later

---
+++ src/main/java/frc/robot/commands/CameraDrive.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class CameraDrive extends CommandBase {
+  /** Creates a new CameraDrive. */
+  public CameraDrive() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    drive.setMotors((0.04)*(m_limelight.getTx()),(-0.04)*(m_limelight.getTx()));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
