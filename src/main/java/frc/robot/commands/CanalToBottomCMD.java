914d9b992d6d4914eca01a2b5001c81d9d646f5f
Georgetheasian <80559769+Georgetheasian@users.noreply.github.com>
2022-02-20 14:20:09 -0500

    Finished Banner sensor code

--- src/main/java/frc/robot/commands/CanalToBottomCMD.java
+++ src/main/java/frc/robot/commands/CanalToBottomCMD.java
@@ -15,6 +15,24 @@ public class CanalToBottomCMD extends CommandBase {
   private IndexSubsystem index;
   private boolean isDone = false;
 
+  // create States
+  public enum States {
+    ONE_BALL_TOP,
+    ONE_BALL_BOTTOM,
+    TWO_BALL,
+    ZERO_BALL,
+  }
+
+  public States initalizeStates() {
+    if (index.readBottomBanner()) {
+      States activeState = States.ONE_BALL_BOTTOM;
+      return activeState;
+    } else {
+      States activeState = States.ZERO_BALL;
+      return activeState;
+    }
+  }
+
   /** Creates a new MegaCommand. */
   public CanalToBottomCMD(CanalSubsystem canalArgs, IndexSubsystem indexArgs) {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -24,26 +42,45 @@ public class CanalToBottomCMD extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (index.readBottomBanner()){
-      isDone = true;
-    } else {
-      isDone = false;
-      canal.setSpeedBack(0.75);
-      canal.setSpeedFront(0.75);
+    isDone = false;
+    switch (initalizeStates()) {
+      case ZERO_BALL:
+        canal.setSpeedBack(0.75);
+        canal.setSpeedFront(0.75);
+        index.setSpeedTower(0);
+        break;
+      case ONE_BALL_BOTTOM:
+        index.setSpeedTower(0.75);
+        canal.setSpeedBack(0.75);
+        canal.setSpeedFront(0.75);
+        break;
+      case ONE_BALL_TOP:
+        canal.setSpeedBack(0.75);
+        canal.setSpeedFront(0.75);
+        index.setSpeedTower(0);
+        break;
+      case TWO_BALL:
+        index.setSpeedTower(0);
+        canal.setSpeedBack(0);
+        canal.setSpeedFront(0);
+        break;
+
     }
+    isDone = true;
+
   }
 
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    canal.setSpeedBack(0);
-    canal.setSpeedFront(0);
+
   }
 
   // Returns true when the command should end.
