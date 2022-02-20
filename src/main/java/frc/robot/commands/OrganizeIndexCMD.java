6573658e2a354d9d0fc335cd65271a5a18fc3f93
Georgetheasian <80559769+Georgetheasian@users.noreply.github.com>
2022-02-20 12:44:52 -0500

    Deleted STATES, resolved according to comments

--- src/main/java/frc/robot/commands/OrganizeIndexCMD.java
+++ src/main/java/frc/robot/commands/OrganizeIndexCMD.java
@@ -25,14 +25,7 @@ public class OrganizeIndexCMD extends CommandBase {
 
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-
-
-
-
-  }
+  
 
 
   // Called once the command ends or is interrupted.
