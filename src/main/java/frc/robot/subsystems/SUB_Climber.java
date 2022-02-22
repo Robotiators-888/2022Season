1d07dab27c71243c77a41bb81b9ee0f024f3d4b7
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-02-22 18:35:06 -0500

    FMC

--- src/main/java/frc/robot/subsystems/SUB_Climber.java
+++ src/main/java/frc/robot/subsystems/SUB_Climber.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import com.revrobotics.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.*;
+
 import frc.robot.Constants;
 
 public class SUB_Climber extends SubsystemBase {
