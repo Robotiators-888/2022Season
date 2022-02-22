8cdff41b6b82d35437f864e5b1efc7c9de27a410
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-02-22 18:31:40 -0500

    Fixed merge conflicts

--- src/main/java/frc/robot/subsystems/SUB_Climber.java
+++ src/main/java/frc/robot/subsystems/SUB_Climber.java
@@ -22,11 +22,7 @@ public class SUB_Climber extends SubsystemBase {
 
   public SUB_Climber() {
     climberMotor = new CANSparkMax(Constants.CLIMBER_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushed);
-<<<<<<< HEAD
     cSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 4, 5);
-=======
-    cSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
->>>>>>> 0e4287e (almost down with climber)
   }
 
   @Override
@@ -40,23 +36,15 @@ public class SUB_Climber extends SubsystemBase {
    * 
    * @param speed Can set either a pos or neg speed for the climber arms
    */
-<<<<<<< HEAD
   public void speedSet(double speed) {
-=======
-  public static void speedSet(double speed) {
->>>>>>> 0e4287e (almost down with climber)
     climberMotor.set(speed);
   }
 
   /**
    * Toggle lock and unlock of the climber
    */
-<<<<<<< HEAD
  
    public void climberLock(){
-=======
-  public void climberLock(){
->>>>>>> 0e4287e (almost down with climber)
     cSolenoid.toggle();
   }
 
@@ -66,10 +54,6 @@ public class SUB_Climber extends SubsystemBase {
    * @param isPistonExtended same value as pistonExtended
    * @return The state of the piston
    */
-<<<<<<< HEAD
-=======
-  
->>>>>>> 0e4287e (almost down with climber)
     public Value lockGet() {
       return (cSolenoid.get());
   
