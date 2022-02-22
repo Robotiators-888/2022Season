050e50b57810e32df734e8e53fa56aa982992e8b
Ryan <ryanpcather@gmail.com>
2022-02-22 13:51:33 -0500

    is finished fix

--- src/main/java/frc/robot/commands/CMD_changeSetpoint.java
+++ src/main/java/frc/robot/commands/CMD_changeSetpoint.java
@@ -33,6 +33,6 @@ public class CMD_changeSetpoint extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return true;
     }
 }
