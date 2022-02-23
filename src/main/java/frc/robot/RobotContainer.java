27caae3c7048d55b5c5bd83c1fa115bef2166ef7
Ryan <ryanpcather@gmail.com>
2022-02-23 07:34:49 -0500

    possible fix for motor output and increased max speed in auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -125,7 +125,8 @@ public class RobotContainer {
 
         Command twoball = new SequentialCommandGroup(
                         new InstantCommand(() -> drivetrain.setPosition(twoBall_p1.getInitialPose())),
-                        autoHelper.getRamset(twoBall_p1), 
+                        autoHelper.getRamset(twoBall_p1),
+                        new InstantCommand(()-> drivetrain.tankDriveVolts(0,0)), 
                         new ParallelRaceGroup(
                                 new ShooterSpin(shoot, -0.50), 
                                 new SequentialCommandGroup(
@@ -136,6 +137,7 @@ public class RobotContainer {
                                 autoHelper.getRamset(twoBall_p2), 
                                 new canalRun(canal, -0.75) , 
                                 new IndexBottomToTopBanner(index, 0.75)),
+                        new InstantCommand(()-> drivetrain.tankDriveVolts(0,0)),
                         new ParallelRaceGroup(
                                 new ShooterSpin(shoot, -0.5),
                                 new SequentialCommandGroup(
