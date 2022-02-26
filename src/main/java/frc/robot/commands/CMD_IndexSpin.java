8f4bc6f57fe77099cc3bba2c17e1381ef960e169
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-02-26 10:08:41 -0500

    FMC

--- src/main/java/frc/robot/commands/CMD_IndexSpin.java
+++ src/main/java/frc/robot/commands/CMD_IndexSpin.java
@@ -4,39 +4,36 @@
 
 package frc.robot.commands;
 
-
+import frc.robot.subsystems.SUB_Intake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Climber;
 
-public class teleopClimber extends CommandBase {
-  private Climber climber;
-  private double triggerSpeed;
-  /** Creates a new teleopClimber. */
-  public teleopClimber(Climber climb, double speed) {
+public class CMD_IndexSpin extends CommandBase {
+  SUB_Intake Intake;
+  double speed;
+
+  /** Creates a new IntakeMotorTest. */
+  public CMD_IndexSpin(SUB_Intake subsystem, double speed) {
     // Use addRequirements() here to declare subsystem dependencies.
-    this.climber = climb;
-    this.triggerSpeed = speed;
-
-    addRequirements(climber);
-    
+    this.Intake = subsystem;
+    this.speed = speed;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-   
-    climber.speedSet(triggerSpeed);
-   
+    Intake.intakeSpeedSet(speed);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    climber.speedSet(0);
+    Intake.intakeSpeedSet(0.0);
   }
 
   // Returns true when the command should end.
