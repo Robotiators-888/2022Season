f6aa5c5b1cdaa62cce4e3fea38a5fb58baca16e4
Ryan <ryanpcather@gmail.com>
2022-02-27 16:27:33 -0500

    functional but inaccurate 2ball

--- src/main/java/frc/robot/commands/IndexBottomToTopBanner.java
+++ src/main/java/frc/robot/commands/IndexBottomToTopBanner.java
@@ -31,7 +31,7 @@ public class IndexBottomToTopBanner extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (!(index.readTopBanner())){
+    if ((index.readTopBanner())){
       isDone = true;
     } else {
       isDone = false;
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
   NetworkTable table;
-  Boolean isHigh = false;
+  Boolean isHigh = true;
 
   public Limelight() {
     table = NetworkTableInstance.getDefault().getTable("limelight");
