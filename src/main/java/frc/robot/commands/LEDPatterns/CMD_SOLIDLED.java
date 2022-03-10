a3a36d195e1142e36ef027ec8ab717e1eca7f783
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-03-10 15:10:21 -0500

    Added functionality, red team red, blue team blue, error, half half

--- src/main/java/frc/robot/commands/LEDPatterns/CMD_SOLIDLED.java
+++ src/main/java/frc/robot/commands/LEDPatterns/CMD_SOLIDLED.java
@@ -9,13 +9,19 @@ import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class CMD_REDLED extends CommandBase {
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;;
+
+public class CMD_SOLIDLED extends CommandBase {
 
   private int counterID = 0;
   private SUB_LED LED;
+  private Alliance color;
+  private int[] rgbVals;
+  private boolean invalid = false;
 
   /** Creates a new CMD_CHASELED. */
-  public CMD_REDLED(SUB_LED LEDArgs) {
+  public CMD_SOLIDLED(SUB_LED LEDArgs) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.LED = LEDArgs;
     addRequirements(LED);
@@ -23,19 +29,49 @@ public class CMD_REDLED extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    color = DriverStation.getAlliance();
+
+    if (color == Alliance.Red){
+      rgbVals[0] = 255;
+      rgbVals[1] = 8;
+      rgbVals[2] = 0;
+    } else if (color == Alliance.Blue){
+      rgbVals[0] = 0;
+      rgbVals[1] = 47;
+      rgbVals[2] = 167;
+    } else{
+      invalid = true;
+    }
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
 
+    if (!invalid){
+      if (counterID>=Constants.LED_LENGTH){
+        counterID = 0;
+      }
+
+      LED.setRGBLED(counterID,rgbVals[0],rgbVals[1],rgbVals[2]);
+
+      counterID++;
+  } else {
+    
     if (counterID>=Constants.LED_LENGTH){
       counterID = 0;
     }
 
-    LED.setRGBLED(counterID,255,8,0);
+    if (counterID<Constants.LED_LENGTH/2){
+      LED.setRGBLED(counterID,255,8,0);
+    } else {
+      LED.setRGBLED(counterID,0,47,167);
+    }
 
     counterID++;
+
+    }
   }
 
   // Called once the command ends or is interrupted.
