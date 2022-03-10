c8557e72823ff24b72f4bc74a7af40898318d6be
888robotiators <ryanpcather@gmail.com>
2022-03-10 15:52:30 -0500

    SPE - added canal mandatory

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -289,7 +289,7 @@ public class RobotContainer {
                 // IntakeSpin(intake, 0.75),new CanalZeroToOneBottom(canal, index)), new
                 // InstantCommand(), intake::intakeGet));
                 L_button4.whenPressed(new SequentialCommandGroup(new InstantCommand(intake::pistonToggle),
-                new ParallelCommandGroup(new InstantCommand(intake::toggleIntakeSpeed), new InstantCommand(canal::toggleCanalSpeed))));
+                new ParallelCommandGroup(new InstantCommand(intake::toggleIntakeSpeed), new InstantCommand(canal::toggleCanalSpeed, canal))));
                                 
                 L_Trigger.whileHeld(new ParallelCommandGroup(new IntakeSpin(intake, 0.75),
                                 new CanalZeroToOneBottom(canal, index)));
