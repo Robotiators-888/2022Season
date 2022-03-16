240fa8a721b9af7408f92a90d045cbbf5bbc539f
Ryan <ryanpcather@gmail.com>
2022-03-16 10:24:10 -0400

    shooter spin up time savings

--- src/main/java/frc/robot/commands/SEQ_dumbShot.java
+++ src/main/java/frc/robot/commands/SEQ_dumbShot.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.Shooter;
