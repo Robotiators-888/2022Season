3d4a12e4795c4612c92bc8cfc69d948e9982c5a5
Ryan <ryanpcather@gmail.com>
2022-03-18 13:53:05 -0400

    a bit of cleanup

--- src/main/java/frc/robot/commands/LimeLight/SEQ_limeShot.java
+++ src/main/java/frc/robot/commands/LimeLight/SEQ_limeShot.java
@@ -3,7 +3,7 @@ package frc.robot.commands.LimeLight;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.indexRun;
+import frc.robot.commands.Index.indexRun;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.Limelight;
