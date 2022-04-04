3281dc1f6b824f8bbe11ca3a255daa8ca257f5fd
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-04-04 16:37:24 -0400

    Fixed auto breaking into teleop

---
+++ src/main/java/frc/robot/commands/Canal/CMD_teleopCanalRun.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Canal;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SUB_Canal;
+
+/** The command canalRun runs the canal belts either both in or both out */
+public class CMD_teleopCanalRun extends CommandBase {
+
+  private SUB_Canal canal;
+  private double speed;
+  /** Creates a new canalRun. */
+  public CMD_teleopCanalRun(SUB_Canal canalArgs, double speedArgs) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.canal = canalArgs;
+    this.speed = speedArgs;
+    addRequirements(canal);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    canal.setSpeedBack(speed);
+    canal.setSpeedFront(speed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    canal.setSpeedFront(0);
+    canal.setSpeedBack(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
